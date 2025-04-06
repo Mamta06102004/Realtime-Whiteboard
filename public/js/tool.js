be41c48565f9f21383929534cb801b86492ed68f
MAMTA <156337073+Mamta06102004@users.noreply.github.com>
2025-04-06 20:18:19 +0530

    added deployment link

--- public/js/tool.js
+++ public/js/tool.js
@@ -1,5 +1,5 @@
 // connect to socket server
-const socket = io.connect("https://pepboard-3fhn.onrender.com");
+const socket = io.connect("https://realtime-whiteboard-indol.vercel.app/");
 // *********************************Basic Setup
 const board = document.querySelector(".board");
 board.height = window.innerHeight;
